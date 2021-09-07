6b309d34595ae54c517e29a1eaf501b7cc586c44
mayrant <mayrant2004@yahoo.com>
2021-09-06 19:38:13 -0500

    created app class component, deleted styles and logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mayra
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      text: "",
+      todos: [],
+      isClicked: false
+    }
+  }
+
+  handleClick = () => {
+    // this.state.isClicked ? 
+    //   this.setState({ isClicked : true }) :
+    //     this.setState({ isClicked : false })
+
+    // this.setState({
+    //   isClicked: !this.state.isClicked,
+    // })    
+  }
+
+  handleChange = (event) => {
+    this.setState({ text: event.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({ todos: [...this.state.todos, this.state.text] })
+    this.setState({ text: "" })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.text} onChange={this.handleChange}></input>
+          <button type="submit">Add Todo</button>
+        </form>
+        <ol>{this.state.todos.map((todo, index) => {
+          return <li key={index}>{todo}</li>
+        })}</ol>
+      </div>
+    );
+  }  
 }
 
 export default App;
